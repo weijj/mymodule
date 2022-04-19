5747fbcb454e6c1913e9bdbb23f7e6933b09984c
weijingjing <weijingjing@didiglobal.com>
2022-04-19 10:10:49 +0800

    add

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/weijj/mymodule"
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,13 @@
+package mymodule
+
+import "fmt"
+
+// Hello says
+func Hello()  {
+	fmt.Println("Hello go mod!")
+}
+
+// Bye says
+func Bye()  {
+	fmt.Println("Bye go mod!")
+}
